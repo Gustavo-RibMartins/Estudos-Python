ed8c7a815dccd52e9e12121e17976c9c279b25f0
Gustavo Ribeiro <guhrm96@gmail.com>
2022-06-20 21:56:15 -0300

    Arquivos completos do curso da Geek University

--- codigos-udemy/003_dir_e_help.py
+++
@@ -1,9 +0,0 @@
-"""
-Utilitarios Python para te auxiliar na programação
-
-dir(tipo de dado/variavel) -> Apresenta todos os atributos/ propriedades e funções/ métodos disponíveis para
-determinado tipo de dado ou variável
-
-help(tipo de dado/ variavel.propriedade) -> Apresenta a documentação/ como utilizar os atributos/ propriedades
-e funções/ métodos disponíveis para determinado tipo de dado ou variável
-"""
